87d77ea449dea9dd7c5a61b7d4534d9be153d63e
Adamfomin-skc <96421014+Adamfomin-skc@users.noreply.github.com>
2022-01-12 10:39:39 +0000

    Add files via upload

---
+++ week 11 coding/making lists 131221.py
@@ -0,0 +1,7 @@
+x = input('Add name of a student: ')
+gender = input('Is',x,'a boy or girl? ' )
+if gender.strip().upper() == 'boy':
+    boy+1
+pupils = []
+pupils.append(x)
+print(pupils)
---
+++ week 11 coding/sallys new dog code 131221.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+Date_Got = datetime.strptime(input('what day did she get the dog? DD/MM/YY '),'%m/%d/%Y') 
+
+Date_Today = datetime.strptime(input('What day is it now? '),m%/)
+
+Lenght_of_Time = (Date_Today- Date_Got)
+
+print(Lenght_of_Time)
